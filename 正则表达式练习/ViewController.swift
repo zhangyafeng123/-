0672f14ff20fda9374bdc517a11090927b8653a4
张亚峰 <1240334911@qq.com>
2018-08-29 20:24:25 +0800

    使用正则表达式从字符串中获取URL链接和文本

--- 正则表达式练习/ViewController.swift
+++ 正则表达式练习/ViewController.swift
@@ -12,14 +12,80 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //正则表达式
+        // . 匹配任意字符，回车除外
+        // * 匹配任意多次
+        // ? 尽量少的匹配
+        
+        //<a href=\"http://app.weibo.com/t/feed/5g0B8s\" rel=\"nofollow\">微博weibo.com</a>
+        
+        let string = "<a href=\"http://app.weibo.com/t/feed/5g0B8s\" rel=\"nofollow\">微博weibo.com</a>"
+        
+        //2.创建正则表达式
+        // pattern - 常说的正则表达式，就是 pattern的写法(匹配方案)
+        // 索引:
+        // 0: 和匹配方案完全一致的字符串
+        // 1: 第一个 () 中的内容
+        // 2: 第二个 () 中的内容
+        // ... 索引从左向右顺序递增
+        //
+        // 对于模糊匹配，如果关心的内容，就使用 (.*?),然后通过索引可以获取结果
+        // 如果不关心的内容, 就是 ` .*? `, 就可以匹配任意的内容
+        let pattern = "<a href=\"(.*?)\" .*?\">(.*?)</a>"
+        
+        //1> 常见正则表达式，如果 pattern 失败，抛出异常
+        guard let regx = try? NSRegularExpression(pattern: pattern, options: []) else {
+            return
+        }
+        //2> 进行查找，两种查找方法
+        // [只找第一个匹配项/查找多个匹配项]
+        guard let result = regx.firstMatch(in: string, options: [], range: NSRange(location: 0, length: string.count)) else {
+            print("没有找到匹配项")
+            return
+        }
+        // 3> result 中只有两个重要的方法
+        // result.numberOfRanges -> 查找到范围的数量
+        // result.range(at: idx) -> 指定 `索引` 位置的范围
+        print("找到的数量\(result.numberOfRanges)")
+        
+        for idx in 0..<result.numberOfRanges {
+            print(result.range(at: idx))
+            
+            let r = result.range(at: idx)
+            let subStr = (string as NSString).substring(with: r)
+            
+            print("下标\(idx)-\(subStr)")
+            
+        }
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 }
 
